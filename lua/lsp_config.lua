59c4e19c701c974368f995a3f811f70db84e353d
Manoj Kumar MC <manojkumarmc@hotmail.com>
2021-11-15 11:23:29 +0530

    modified the symbol

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -63,8 +63,8 @@ end
 -- }}}
 
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    -- virtual_text = {prefix = ''},
-    virtual_text = true,
+    virtual_text = {prefix = '◂'},
+    -- virtual_text = true,
     signs = true,
     underline = true,
     update_in_insert = false,
