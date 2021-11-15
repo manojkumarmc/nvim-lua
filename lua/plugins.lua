0510a93e91e370d95b154307cf8c6de02b5217df
mmyali26 <mmyali26@ocean.ibm.com>
2021-11-15 15:35:38 +0530

    tab changes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -349,23 +349,44 @@ return require('packer').startup(function(use)
                     ["<C-u>"] = cmp.mapping.scroll_docs(4),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-c>"] = cmp.mapping.close(),
-                    ['<CR>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true, },
+                    ["<CR>"] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true, },
+                    -- ["<Tab>"] = cmp.mapping(function(fallback)
+                    --   if cmp.visible() then
+                    --     cmp.select_next_item()
+                    --   elseif vim.fn["vsnip#available"]() == 1 then
+                    --     feedkey("<Plug>(vsnip-expand-or-jump)", "")
+                    --   elseif has_words_before() then
+                    --     cmp.complete()
+                    --   else
+                    --     fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+                    --   end
+                    -- end, { "i", "s" }),
+                    -- ["<S-Tab>"] = cmp.mapping(function()
+                    --   if cmp.visible() then
+                    --     cmp.select_prev_item()
+                    --   elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+                    --     feedkey("<Plug>(vsnip-jump-prev)", "")
+                    --   end
+                    -- end, { "i", "s" }),
                     ["<Tab>"] = cmp.mapping(function(fallback)
                       if cmp.visible() then
                         cmp.select_next_item()
-                      elseif vim.fn["vsnip#available"]() == 1 then
-                        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+                      elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
                       elseif has_words_before() then
                         cmp.complete()
                       else
-                        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+                        fallback()
                       end
                     end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function()
+
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
                       if cmp.visible() then
                         cmp.select_prev_item()
-                      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-                        feedkey("<Plug>(vsnip-jump-prev)", "")
+                      elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                      else
+                        fallback()
                       end
                     end, { "i", "s" }),
                     ['<C-l>'] = cmp.mapping(function()
